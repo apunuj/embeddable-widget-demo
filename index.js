afbc464c37f9b1dfd204dc6c5a5e19243d4afeb8
Apoorv Kesharwani <kesharwani.apoorv@gmail.com>
2017-09-18 14:39:16 +0530

    Code for serving the platform script file

--- index.js
+++ index.js
@@ -63,3 +63,39 @@ serverApp.use(function(req, res, next) {
   }
   next();
 });
+
+//Serving the platform script file
+serverApp.engine('js', ejs.renderFile);
+serverApp.get(platformScript, function(req, res) {
+  res.render('server'+platformScript, {
+    serverHost: serverHost,
+    platformScript: platformScript
+  });
+});
+
+console.log('hello from server');
+
+(function(global) {
+  var serverHost = '<%= serverHost %>';
+  var elements = document.querySelectorAll('.foo-widget');
+  for (var i = 0; i < elements.length; ++i) {
+    el = elements[i];
+    processElement(el);
+  }
+
+  function processElement(el) {
+    var id = el.getAttribute('data-foo-id');
+    var processed = el.getAttribute('data-foo-processed');
+    if (!id || processed === 'done') {
+      console.log('skipping element:', el);
+      return;
+    }
+    var xhr = newXMLHttpRequest();
+    xhr.onload = function() {
+      el.innerHTML = 'foo-widget: '+this.responseText;
+      el.setAttribute('data-foo-processed', 'done');
+    };
+    xhr.open("GET", serverHost+'/api/3rd/foo-widget/init/'+id);
+    xhr.send();
+  }
+})();
