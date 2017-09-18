e6934ef46922f7d36d3824c4d80b7e9ee10ec8d6
Apoorv Kesharwani <kesharwani.apoorv@gmail.com>
2017-09-18 15:07:10 +0530

    Final widget code for the page dom method

--- index.js
+++ index.js
@@ -73,33 +73,7 @@ serverApp.get(platformScript, function(req, res) {
   });
 });
 
-console.log('hello from server');
-
-(function(global) {
-  var serverHost = '<%= serverHost %>';
-  var elements = document.querySelectorAll('.foo-widget');
-  for (var i = 0; i < elements.length; ++i) {
-    el = elements[i];
-    processElement(el);
-  }
-
-  function processElement(el) {
-    var id = el.getAttribute('data-foo-id');
-    var processed = el.getAttribute('data-foo-processed');
-    if (!id || processed === 'done') {
-      console.log('skipping element:', el);
-      return;
-    }
-    var xhr = newXMLHttpRequest();
-    xhr.onload = function() {
-      el.innerHTML = 'foo-widget: '+this.responseText;
-      el.setAttribute('data-foo-processed', 'done');
-    };
-    xhr.open("GET", serverHost+'/api/3rd/foo-widget/init/'+id);
-    xhr.send();
-  }
-})();
-
+//Respond to API requests from widgets
 serverApp.get('/api/3rd/foo-widget/init/:id', function(req, res) {
   var id = req.params.id;
   res.send('api response #'+id);
