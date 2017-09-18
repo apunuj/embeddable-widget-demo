9984fdbe358cedbcc3192512e99a72cb247c4557
Apoorv Kesharwani <kesharwani.apoorv@gmail.com>
2017-09-18 13:45:30 +0530

    Added the functionality to detect server URL

--- index.js
+++ index.js
@@ -11,3 +11,23 @@ var clientApp = express();
 
 http.createServer(serverApp).listen(serverPort);
 http.createServer(clientApp).listen(clientPort);
+
+
+var networkInterfaces = os.networkInterfaces();
+
+function getIpAddress() {
+  var keys = Object.keys(networkInterfaces);
+  for (var x = 0; x < keys.length; ++x) {
+    var netIf = networkInterfaces[keys[x]];
+    for (var y = 0; y < netIf.length; ++y) {
+      var addr = netIf[y];
+      if (addr.family === 'IPv4' && !addr.internal) {
+        return addr.address;
+      }
+    }
+  }
+  return '127.0.0.1';
+}
+
+var serverHost = '//'+getIpAddress()+':'+serverPort;
+var platformScript = '/3rd/platform.js';
